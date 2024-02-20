a2445a85f755a2ffe589ebbc45e9a302358250b8
dagayugi <yoshioka.ta97@gmail.com>
2024-02-20 23:42:13 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+[The Rust Programming Language 日本語版-12](https://doc.rust-jp.rs/book-ja/ch12-00-an-io-project.html)
+
+- [x] テストケースの追加
+- [x] 引数にCASE_INSENSITIVEの指定がある場合に環境変数より引数を優先する
---
+++ src/lib.rs
@@ -0,0 +1,175 @@
+use std::error::Error;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        let mut case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        if args.len() > 3 {
+            case_sensitive = match args[3].to_lowercase().as_str() {
+                "true" => true,
+                "false" => false,
+                _ => case_sensitive,
+            };
+        }
+
+        Ok(Config { query, filename , case_sensitive})
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insenstive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insenstive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use std::vec;
+
+    use super::*;
+
+    const MINIGREP: &str = "minigrep";
+    const QUERY: &str = "query";
+    const FILENAME: &str = "poem.txt";
+
+    #[test]
+    fn case_new() {
+        let args = vec![String::from(MINIGREP), String::from(QUERY), String::from(FILENAME)];
+        let config = Config::new(&args).unwrap();
+        assert_eq!(config.query, QUERY);
+        assert_eq!(config.filename, FILENAME);
+        assert_eq!(config.case_sensitive, true);
+    }
+
+    #[test]
+    fn case_new_sensitive_from_args() {
+        let args = vec![String::from(MINIGREP), String::from(QUERY), String::from(FILENAME), String::from("true")];
+        let config = Config::new(&args).unwrap();
+        assert_eq!(config.query, QUERY);
+        assert_eq!(config.filename, FILENAME);
+        assert_eq!(config.case_sensitive, true);
+    }
+
+    #[test]
+    fn case_new_with_no_args() {
+        let args = vec![String::from(MINIGREP)];
+        let config = Config::new(&args);
+        assert!(config.is_err());
+    }
+
+    #[test]
+    fn case_new_with_case_no_env() {
+        let args = vec![String::from(MINIGREP), String::from("query"), String::from(FILENAME)];
+        let config = Config::new(&args).unwrap();
+        assert_eq!(config.case_sensitive, true);
+    }
+
+    #[test]
+    fn case_new_with_case_env() {
+        env::set_var("CASE_INSENSITIVE", "1");
+        let args = vec![String::from(MINIGREP), String::from("query"), String::from(FILENAME)];
+        let config = Config::new(&args).unwrap();
+        assert_eq!(config.case_sensitive, false);
+        env::remove_var("CASE_INSENSITIVE");
+    }
+
+    #[test]
+    fn case_run() {
+        let args = vec![String::from(MINIGREP), String::from("query"), String::from(FILENAME)];
+        let config = Config::new(&args).unwrap();
+        let result = run(config);
+
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn case_run_with_none_file() {
+        let args = vec![String::from(MINIGREP), String::from(QUERY), String::from("none")];
+        let config = Config::new(&args).unwrap();
+
+        let result = run(config);
+
+        assert!(result.is_err());
+
+    }
+
+    #[test]
+    fn case_senstive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+        assert_eq!(vec!["safe, fast, productive."],
+        search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insenstive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insenstive(query, contents)
+        );
+    }
+}
